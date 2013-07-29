15b81b950bc80ee29a23b3e76041c72ab1017ef0
U-Somanchi\Sriram <somanchi@cmu.edu>
2013-07-29 15:47:23 -0500

    Somanchi: Initial analysis year by year

--- code/initial_analysis.py
+++ code/initial_analysis.py
@@ -13,9 +13,10 @@ readline.parse_and_bind('tab: complete')
 
 labs = pd.read_csv('/glusterfs/users/HVA/HVA_data/dr_6988_labs_mod.csv')
 adm_dis_df = pd.read_csv('/glusterfs/users/HVA/HVA_data/dr_6988_final_enc_mod.csv')
+vitals = pd.read_csv('/glusterfs/users/HVA/HVA_data/dr_6988_final_vitals.csv')
 med_clar_df = pd.read_csv('/glusterfs/users/HVA/HVA_data/dr_6988_final_med_clarity_mod.csv')
 med_cent_df = pd.read_csv('/glusterfs/users/HVA/HVA_data/dr_6988_final_med_centricity_mod.csv')
-vitals = pd.read_csv('/glusterfs/users/HVA/HVA_data/dr_6988_final_vitals.csv')
+
 
 adm_dis_df['adm_date'] = pd.to_datetime(adm_dis_df['adm_date'])
 adm_dis_df['dsc_date'] = pd.to_datetime(adm_dis_df['dsc_date'])
@@ -261,6 +262,150 @@ case_plot, = plt.plot(xs,labs_per_visit_density(xs))
 plt.xlabel('Number of Lab Tests After Visit')
 plt.ylabel('Density')
 plt.title('Number of Lab Tests After visit distribution of the patients in the hospital')
+plt.show()
+pdf_pages.savefig(fig)
+pdf_pages.close()
+
+###############################################
+# Analysis of patient labs before first admit
+###############################################
+
+adm_dis_pat_group = adm_dis_df.groupby(['pat_id'])
+first_adm_date_pat = adm_dis_pat_group['adm_date'].min()
+
+num_rows = len(first_adm_date_pat)
+labs_before_first_adm = np.ndarray( shape= (num_rows,), dtype= [('pat_id', np.int64), ('before_first_num', np.int64), ('before_first_enc_num', np.int64)])
+labs_before_first_adm[:]['before_first_num'] = 0
+labs_before_first_adm[:]['before_first_enc_num'] = 0
+vitals_before_first_adm = np.ndarray( shape= (num_rows,), dtype= [('pat_id', np.int64), ('before_first_num', np.int64), ('before_first_enc_num', np.int64)])
+vitals_before_first_adm[:]['before_first_num'] = 0
+vitals_before_first_adm[:]['before_first_enc_num'] = 0
+
+labs_group_pat = labs.groupby(['pat_id'])
+vitals_group_pat = vitals.groupby(['pat_id'])
+labs
+cur_row = 0
+for pat_id, group in labs_group_pat:
+    try:
+        adm_date = first_adm_date_pat.ix[pat_id]
+    except Exception as e:
+        continue
+    before_num = np.sum(group['result_time'] < adm_date)
+    before_enc_num = len(group[group['result_time']<adm_date]['enc_id'].unique())
+    #print before_num, before_enc_num
+    #sys.stdin.readline(1)
+    labs_before_first_adm[cur_row]['pat_id'] = pat_id
+    labs_before_first_adm[cur_row]['before_first_num'] = before_num
+    labs_before_first_adm[cur_row]['before_first_enc_num'] = before_enc_num
+    cur_row = cur_row+1
+labs_before_first_adm_df = pd.DataFrame(labs_before_first_adm)
+
+pdf_pages = PdfPages('Before_first_encounters.pdf')
+fig =plt.figure()
+labs_per_visit_density = gaussian_kde(num_labs_per_visit)
+#xs = np.linspace(0,np.max(num_labs_per_visit),200)
+xs = np.linspace(0,250,200)
+labs_per_visit_density.covariance_factor = lambda : .25
+labs_per_visit_density._compute_covariance()
+case_plot, = plt.plot(xs,labs_per_visit_density(xs))
+#plt.hist(readmission_freq)
+plt.xlabel('Number of Lab Tests Before Visit')
+plt.ylabel('Density')
+plt.title('Number of Lab Tests Before visit distribution of the patients in the hospital')
+plt.show()
+pdf_pages.savefig(fig)
+pdf_pages.close()
+
+
+cur_row = 0
+for pat_id, group in vitals_group_pat:
+    try:
+        adm_date = first_adm_date_pat.ix[pat_id]
+    except Exception as e:
+        continue
+    before_num = np.sum(group['flo_meas_time'] < adm_date)
+    before_enc_num = len(group[group['flo_meas_time']<adm_date]['enc_id'].unique())
+    vitals_before_first_adm[cur_row]['pat_id'] = pat_id
+    vitals_before_first_adm[cur_row]['before_first_num'] = before_num
+    vitals_before_first_adm[cur_row]['before_first_enc_num'] = before_enc_num
+    cur_row = cur_row+1
+vitals_before_first_adm_df = pd.DataFrame(vitals_before_first_adm)
+
+
+##################################################################
+# YEAR BY YEAR ANALYSIS
+##################################################################
+
+
+# ANALYSIS ON THE NUMBER OF ADMISSIONS PER YEAR
+years = [2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013]
+num_adm = []
+
+for year in years:
+    adm_dis_year =  adm_dis_df[[x == year for x in [x.year for x in adm_dis_df['adm_date']]]]
+    num_adm.append(len(adm_dis_year['pat_id'].unique()))
+
+pdf_pages = PdfPages('Adm_per_year.pdf')
+fig = plt.figure()
+width =1
+indexes = np.arange(len(num_adm))
+plt.bar(indexes,num_adm,width)
+plt.xticks(np.arange(len(years))+ width*0.5, years)
+plt.xlabel('Year')
+plt.ylabel('Number of unique patients in a given year')
+plt.title('Histogram of number of patients getting admitted in a given year')
+plt.show()
+pdf_pages.savefig(fig)
+pdf_pages.close()
+
+# ANALYSIS ON ADMISSIONS IN THE GIVEN YEAR WITHOUT ANY PREVIOUS ADMISSIONS
+
+# None admitted in year 2003 and hence keeping patient id is empty
+pat_years = { '2003':[]}
+num_adm_wo_prev = []
+for year in years:
+    a = pat_years.values()
+    prev_year_pats = list(itertools.chain(*a))
+    adm_dis_year = adm_dis_df[[x == year for x in [x.year for x in adm_dis_df['adm_date']]]]
+    this_year_pats = adm_dis_year[ ~(adm_dis_year['pat_id'].isin(prev_year_pats))]['pat_id'].unique()
+    pat_years[str(year)] = this_year_pats
+    num_adm_wo_prev.append(len(this_year_pats))
+
+pdf_pages = PdfPages('Adms_without_prev_adm_per_year.pdf')
+fig = plt.figure()
+width =1
+indexes = np.arange(len(num_adm_wo_prev))
+plt.bar(indexes,num_adm_wo_prev,width)
+plt.xticks(np.arange(len(years))+ width*0.5, years)
+plt.xlabel('Year')
+plt.ylabel('Number of unique patients in a given year')
+plt.title('Hist of num of pats admitted in a given year w/o any prev adm')
+plt.show()
+pdf_pages.savefig(fig)
+pdf_pages.close()
+
+
+#ANALYSIS ON THE LAB DATA WITH NEW ADMISSIONS ON A GIVEN YEAR
+enc_labs_before_year = {'2003':[]}
+num_enc_labs_before_year = []
+for year in years:
+    pats_this_year = pat_years[str(year)]
+    labs_this_year_pats = labs[ labs['pat_id'].isin(pats_this_year)]
+    labs_before_this_year = labs_this_year_pats[ [x<year for x in [x.year for x in labs_this_year_pats['result_time']]]]
+    temp = labs_before_this_year['enc_id'].unique()
+    enc_labs_before_year[str(year)] = temp
+    num_enc_labs_before_year.append(len(temp))
+
+
+pdf_pages = PdfPages('Num_encounters_before_year.pdf')
+fig = plt.figure()
+width =1
+indexes = np.arange(len(num_enc_labs_before_year))
+plt.bar(indexes,num_enc_labs_before_year,width)
+plt.xticks(np.arange(len(years))+ width*0.5, years)
+plt.xlabel('Year')
+plt.ylabel('Number of encounters before year with first time admission on that year')
+plt.title('Hist of encounters before year with first time admission on that year')
 plt.show()
 pdf_pages.savefig(fig)
 pdf_pages.close()
